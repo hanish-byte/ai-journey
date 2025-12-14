c32cc568dd5fec53f8b43a9675e968d1fe70bbba
hanish-byte <hanishjabilli143@gmail.com>
2025-12-14 23:15:06 +0530

    Add study schedule for specified number of days

---
+++ day4.py
@@ -0,0 +1,6 @@
+days = int(input("How many days do you want to study? "))
+
+for day in range(1, days + 1):
+    print("Day", day, ": Study AI for 1 hour 🚀")
+
+print("Consistency is more important than motivation 💪")
