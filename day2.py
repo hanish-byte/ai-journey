ab19fdbce4de2fe1b3f971d3eef9172de1621e94
hanish-byte <hanishjabilli143@gmail.com>
2025-12-14 22:47:47 +0530

    Add user input and eligibility check
    Fix input prompt and age eligibility message.

---
+++ day2.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter ypur age: ")
+
+print("Hello", name)
+print("You are", age, "years old")
+
+if age >=19:
+  print("Ypu are eligiblr to learn advanced AI 🚀")
+          else:
+print("Start with basics and grow step by step 💪")
